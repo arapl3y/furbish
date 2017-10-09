bae0df48adc0a991dab69ddde6b58e089c3fe7ad
arapl3y <rapley3@gmail.com>
2017-10-09 20:24:21 +1100

    Add vue-cli command using MF template

--- scripts/vue2.sh
+++ scripts/vue2.sh
@@ -4,8 +4,6 @@ GREEN='\033[0;32m'
 BLUE='\033[1;34m'
 ERR="\033[1;31m"
 NC='\033[0m'
-PACKAGES="axios vee-validate vuex"
-# DEV_PACKAGES="esdoc eslint eslint-config-airbnb eslint-plugin-import eslint-plugin-jsx-a11y@5"
 
 echo 
 echo "${GREEN}Furbishing a Vue 2 project${NC}"
@@ -16,7 +14,6 @@ if ! type "node" > /dev/null; then
     echo "Please follow the install instructions at https://nodejs.org/en/ then try again${NC}" 1>&2
     exit
 fi
-
 
 if ! type "vue" > /dev/null; then
     echo "${ERR} Vue CLI not found!" 
@@ -29,83 +26,23 @@ fi
 read -p "What would you like your app to be called? " appname
 
 echo
-echo "${BLUE}Furbish:${NC} Initialising Vue project using Webpack template"
+echo "${BLUE}Furbish:${NC} Initialising Vue project using MF template"
 echo
 
-vue init webpack $appname
+vue init MentallyFriendly/vue2-boilerplate $appname
 cd ./$appname
-
 
 if ! type yarn > /dev/null; then
     echo
-    echo "${BLUE}Furbish:${NC} Installing initial dependencies with npm"
-    echo
-
-    npm install
-
-    echo
-    echo "${BLUE}Furbish:${NC} Installing additional dependencies: $PACKAGES"
-    echo
-
-    npm install --save $PACKAGES
+    echo "${ERR}Yarn not found!" 1>&2
+    echo "Please follow the install instructions at https://yarnpkg.com/en/docs/install then try again${NC}" 1>&2
+    exit
 else
     echo
     echo "${BLUE}Furbish:${NC} Installing initial dependencies with yarn"
     echo
     yarn install
-    echo
-    echo "${BLUE}Furbish:${NC} Installing additional dependencies: $PACKAGES"
-    echo
-    yarn add $PACKAGES
 fi
-
-# Set up file architecture
-echo
-echo "${BLUE}Furbish:${NC} Setting up file architecture"
-echo
-
-cd ./src
-rm assets/logo.png components/Hello.vue App.vue
-
-cat << EOF > App.vue
-<template>
-  <div id="app">
-    <h1>Hello</h1>
-  </div>
-</template>
-
-<script>
-export default {
-  name: 'app',
-};
-</script>
-
-<style>
-
-</style>
-EOF
-
-if [ -d ./router ] 
-then
-    cd ./router
-    rm index.js
-    cat << EOF > index.js
-    import Vue from 'vue'
-    import Router from 'vue-router'
-
-    Vue.use(Router)
-
-    export default new Router({
-        routes: []
-    })
-EOF
-fi
-
-# TODO: Add .editorconfig and .eslintrc
-
-# Return to root directory
-cd ..
-
 
 echo 
 echo "${GREEN}SUCCESS! Your project directory is fully furbished! ${NC}"
@@ -114,8 +51,4 @@ echo "${GREEN}Booting up dev server now...${NC}"
 echo 
 
 # Start dev server
-if ! type yarn > /dev/null; then
-   npm start
-else
-   yarn start
-fi
+yarn start
